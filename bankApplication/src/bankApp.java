0c204f810827148dc16a2c044730e09e9c445f9c
Vivek Patel <31905378+vick0123@users.noreply.github.com>
2022-01-16 19:59:36 -0500

    Add files via upload

---
+++ bankApplication/src/bankApp.java
@@ -0,0 +1,13 @@
+
+public class bankApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account vivek = new Account("Vivek Patel", "A1313");
+		vivek.showMenu();
+		
+		
+	}
+
+}
